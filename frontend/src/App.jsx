31add79cf5801af2f012218b9b4367a3efc38517
Rahi Modi <45651940+RuhiModi@users.noreply.github.com>
2026-02-24 13:46:38 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,32 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
+import Campaigns from './pages/Campaigns'
+import CreateCampaign from './pages/CreateCampaign'
+import CampaignDetail from './pages/CampaignDetail'
+import Settings from './pages/Settings'
+import Layout from './components/Layout'
 
+function PrivateRoute({ children }) {
+  const token = localStorage.getItem('token')
+  return token ? children : <Navigate to="/login" replace />
+}
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Toaster position="top-right" />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<PrivateRoute><Layout /></PrivateRoute>}>
+          <Route index element={<Dashboard />} />
+          <Route path="campaigns" element={<Campaigns />} />
+          <Route path="campaigns/new" element={<CreateCampaign />} />
+          <Route path="campaigns/:id" element={<CampaignDetail />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
